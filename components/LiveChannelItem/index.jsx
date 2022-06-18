dcfa5845a1de63dd30d5e7e947a745d0d3b03891
EduardoR99 <102009964+EduardoR99@users.noreply.github.com>
2022-06-18 07:39:52 -0300

    changing props

--- components/LiveChannelItem/index.jsx
+++ components/LiveChannelItem/index.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const LiveChannelItem = ({img, profile_img, title, user, game}) => {
   return (
     <div className='p-2'>
-        <Image src={img} />
+        <Image src={img} alt='/'/>
         <div className='flex pt-2'>
             <div>
                 <Image className='rounded-full' src={profile_img} alt='/' width='60' height='60'/>
